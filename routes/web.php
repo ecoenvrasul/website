f21a580589f4a0442e0993eab3842767dfcaffa6
Kadirov Rasul <qadirov_0308@mail.ru>
2023-01-24 08:59:55 +0500

    No change

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('employee', [EmployeeController::class, 'index']);
+
+
 Route::get('/route', function(){
     return view('main');
 });
